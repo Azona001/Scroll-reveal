318b67b9577cc1079c60deff42f619d5fa64c9f8
Azona Isagba <isagbaazona@gmail.com>
2025-06-13 19:18:02 -0700

    added files for website

---
+++ index.js
@@ -0,0 +1,23 @@
+const revealElements = document.querySelectorAll('[data-reveal]');
+
+const scrollReveal = function () {
+    for (let i = 0, len = revealElements.length; i < len; i++) {
+        const isElementsOnScreen = revealElements[i].getBoundingClientRect().top < window.innerHeight;
+    
+        if (isElementsOnScreen) {
+            revealElements[i].classList.add('revealed');
+        }else{
+            revealElements[i].classList.remove('revealed');
+        }
+    }
+
+}
+
+window.addEventListener('scroll', scrollReveal);
+window.addEventListener('load', scrollReveal);
+
+const menu = document.querySelector('.menu');
+menu.addEventListener('click', () => {
+    const menuList = document.querySelector('.menulist');
+    menuList.classList.toggle('pop');
+});
